2bdda8ea22f10d111680f5198fd6659d4785f72d
Pavel Kurskiy <>
2020-02-12 23:52:27 +0300

    going live with new server

--- src/generated-conf/config.php
+++ src/generated-conf/config.php
@@ -4,12 +4,12 @@ $serviceContainer->checkVersion('2.0.0-dev');
 $serviceContainer->setAdapterClass('default', 'mysql');
 $manager = new \Propel\Runtime\Connection\ConnectionManagerSingle();
 $manager->setConfiguration(array (
-//  'dsn' => 'mysql:host=localhost;port=3306;dbname=u0859476_crawler',
-//  'user' => 'u0859476_crawler',
-//  'password' => '8M3x2I4f',
-    'dsn' => 'mysql:host=127.0.0.1;port=8889;dbname=u0859476_crawler',
-    'user' => 'root',
-    'password' => 'root',
+  'dsn' => 'mysql:host=localhost;port=3306;dbname=u0952196_crawler',
+  'user' => 'u0952196_crawler',
+  'password' => '8M3x2I4f',
+//    'dsn' => 'mysql:host=127.0.0.1;port=8889;dbname=u0952196_crawler',
+//    'user' => 'root',
+//    'password' => 'root',
 
   'settings' =>
   array (
--- src/propel.php
+++ src/propel.php
@@ -5,12 +5,12 @@ return [
             'connections' => [
                 'default' => [
                     'adapter' => 'mysql',
-//                    'dsn' => 'mysql:host=localhost;port=3306;dbname=u0859476_crawler',
-//                    'user' => 'u0859476_crawler',
-//                    'password' => '8M3x2I4f',
-                    'dsn' => 'mysql:host=127.0.0.1;port=8889;dbname=u0859476_crawler',
-                    'user' => 'root',
-                    'password' => 'root',
+                    'dsn' => 'mysql:host=localhost;port=3306;dbname=u0952196_crawler',
+                    'user' => 'u0952196_crawler',
+                    'password' => '8M3x2I4f',
+//                    'dsn' => 'mysql:host=127.0.0.1;port=8889;dbname=u0952196_crawler',
+//                    'user' => 'root',
+//                    'password' => 'root',
                     'settings' => [
                         'charset' => 'utf8'
                     ]
